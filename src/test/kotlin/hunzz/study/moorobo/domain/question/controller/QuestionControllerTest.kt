6e834660528830ee9f04073f93300d4337edbd48
hunzz <hunzz.study@gmail.com>
2024-06-12 11:50:12 +0900

    [Refactor] QuestionController의 테스트 코드 리팩토링
    - json 형식의 request를 문자열로 직접 입력하지 않고, DTO 객체와 ObjectMapper를 활용하여 대입
    - 불필요 테스트 케이스 제거

--- src/test/kotlin/hunzz/study/moorobo/domain/question/controller/QuestionControllerTest.kt
+++ src/test/kotlin/hunzz/study/moorobo/domain/question/controller/QuestionControllerTest.kt
@@ -1,15 +1,18 @@
 package hunzz.study.moorobo.domain.question.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import hunzz.study.moorobo.domain.question.dto.AddQuestionRequest
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,64 +20,56 @@ class QuestionControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
     @Test
     @DisplayName("정상적으로 질문이 등록되는 경우")
     fun addQuestion() {
+        val json = objectMapper.writeValueAsString(
+            AddQuestionRequest(title = "테스트 제목", content = "테스트 내용")
+        )
+
         mockMvc.perform(
             post("/questions")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    """
-                        {
-                          "title": "테스트 제목",
-                          "content": "테스트 내용"
-                        }    
-                    """.trimIndent()
-                )
+                .contentType(APPLICATION_JSON)
+                .content(json)
         ).andExpect(status().isCreated)
+            .andExpect(jsonPath("$.id").value(1L))
             .andExpect(jsonPath("$.title").value("테스트 제목"))
             .andExpect(jsonPath("$.content").value("테스트 내용"))
             .andDo(print())
     }
 
     @Test
-    @DisplayName("질문의 제목과 내용을 미입력한 경우")
+    @DisplayName("질문의 내용을 미입력한 경우")
     fun addQuestionException1() {
+        val json = objectMapper.writeValueAsString(
+            AddQuestionRequest(title = "테스트 제목", content = "")
+        )
+
         mockMvc.perform(
             post("/questions")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    """
-                        {
-                          "title": "",
-                          "content": ""
-                        }    
-                    """.trimIndent()
-                )
+                .contentType(APPLICATION_JSON)
+                .content(json)
         ).andExpect(status().isBadRequest)
             .andExpect(jsonPath("$.errors").isArray)
-            .andExpect(jsonPath("$.errors.size()").value(2))
-            .andExpect(jsonPath("$.errors[0].field").value("title"))
-            .andExpect(jsonPath("$.errors[0].message").value("질문의 제목은 필수 입력 항목입니다."))
-            .andExpect(jsonPath("$.errors[1].field").value("content"))
-            .andExpect(jsonPath("$.errors[1].message").value("질문의 내용은 필수 입력 항목입니다."))
-            .andDo(print())
+            .andExpect(jsonPath("$.errors.size()").value(1))
+            .andExpect(jsonPath("$.errors[0].field").value("content"))
+            .andExpect(jsonPath("$.errors[0].message").value("질문의 내용은 필수 입력 항목입니다."))
     }
 
     @Test
     @DisplayName("128자를 초과하는 질문 제목이 등록된 경우")
     fun addQuestionException2() {
+        val json = objectMapper.writeValueAsString(
+            AddQuestionRequest(title = "테스트".repeat(43), content = "테스트 내용")
+        )
+
         mockMvc.perform(
             post("/questions")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    """
-                        {
-                          "title": "${"테스트".repeat(43)}",
-                          "content": "테스트 내용"
-                        }    
-                    """.trimIndent()
-                )
+                .contentType(APPLICATION_JSON)
+                .content(json)
         ).andExpect(status().isBadRequest)
             .andExpect(jsonPath("$.errors").isArray)
             .andExpect(jsonPath("$.errors.size()").value(1))
